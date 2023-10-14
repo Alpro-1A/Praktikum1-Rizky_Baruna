d4e7a6d5d5deac7abdcbaff15be0ab8994762ac1
RizBarr <2310631170115@student.unsika.ac.id>
2023-10-14 22:17:18 +0700

    Rizky Baruna

---
+++ Pertemuan-1/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream> // input output stream
+
+using namespace std;
+
+int main()
+{
+    cout << "......................................................................................" << endl << endl;
+    cout << "Nama: Rizky Baruna" << endl << endl;
+    cout << "NPM: 2310631170115" << endl << endl;
+    cout << "Kelas: 1A Informatika" << endl << endl;
+    cout << "Cita-Cita: Programmer" << endl << endl;
+    cout << "Disuruh Ortu, Prospek kerjanya luas, Tertarik untuk belajar hal-hal terkait teknologi" << endl << endl;
+    cout << "......................................................................................" << endl << endl;
+    return 0;
+}
